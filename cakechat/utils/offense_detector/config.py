ec07f711c3f1032c684bb24abf8a275bce6c4d7d
Khalman Mikhail <khalman.m@gmail.com>
2018-03-23 15:59:21 +0300

    fix path to offense-detector data

--- cakechat/utils/offense_detector/config.py
+++ cakechat/utils/offense_detector/config.py
@@ -4,4 +4,4 @@ import pkg_resources
 import cakechat.utils.offense_detector
 
 OFFENSIVE_PHRASES_PATH = pkg_resources.resource_filename(cakechat.utils.offense_detector.__name__,
-                                                         '/data/offensive_phrases.csv')
+                                                         os.path.join('data', 'offensive_phrases.csv'))
